3cb436cadeb152218a5d3427e7f1cdc90b8b72c1
Duckling Dark <paul.panda.969@gmail.com>
2018-12-13 23:37:10 +0300

    Added folder with c# files for real_time widget

---
+++ c#/real_time_sharp/real_time_sharp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace real_time_sharp {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            string[] args = Environment.GetCommandLineArgs();
+            string connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("meteo_station");
+            var collection = database.GetCollection<SensorData>("outside");
+            var filter = Builders<SensorData>.Filter.Eq("sensor", "BMP Pressure ");
+
+            var result = collection
+                        .Find(filter)
+                        .Limit(1)
+                        .Sort(Builders<SensorData>.Sort.Descending(x => x._id))
+                        .ToList();
+
+            foreach (var rs in result) {
+
+            }
+        }
+
+        private void btn_Exit_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+    }
+}
---
+++ c#/real_time_sharp/real_time_sharp/Mongo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+using IniParser;
+using IniParser.Model;
+
+namespace real_time_sharp {
+    public class SensorData {
+        public ObjectId _id { get; set; }
+        public string sensor { get; set; }
+        public string data { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+    }
+
+    class Mongo {
+        private FileIniDataParser parser;
+
+        public void connect(string ini_Path) {
+            parser = new FileIniDataParser();
+            IniData data = parser.ReadFile("..\\..\\..\\python\\configs\\");
+        }
+
+        //public SensorData read_sensor(string sensor) {
+
+        //}
+
+        public void disconnect() {
+
+        }
+    }
+}
